6434f584bb5c2d8ee1d6277e657595227d6db43e
doanduyvan <duyvanlee2001@gmail.com>
2025-03-05 23:54:45 +0700

    update header

--- frontend/user/src/components/header/header.jsx
+++ frontend/user/src/components/header/header.jsx
@@ -3,7 +3,7 @@ function Header(){
 
     return(
         <>
-            <header className="fixed w-full">
+            <header className="fixed w-full z-10">
                 <div className="swapper p-3">
                     <div className="border-[1px] border-[#141313]">
                         {/* <nav></nav> */}
